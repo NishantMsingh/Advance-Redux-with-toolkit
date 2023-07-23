df9d06e4784f5a7264ef7d5d14d850694cf04f04
NisantMsingh <nishantkumarsingh16019@gmail.com>
2023-07-24 02:09:15 +0530

    fetch and notification

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+
  
 function App() {
   const showCart = useSelector(state=>state.ui.cartIsVisible);
+  const cart=useSelector(state=>state.cart);
+  useEffect(()=>{
+    fetch("https://redux-fe1aa-default-rtdb.firebaseio.com/cart.json",{
+      method:"PUT",
+      body:JSON.stringify(cart),
+    });
+  },[cart]);
   return (
     <Layout>
       {showCart && <Cart />}
